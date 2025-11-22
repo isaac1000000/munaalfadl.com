6bd10ca225bb548886f71a393c6726d9ee1dc99f
isaac1000000 <isaac.fisher@nyu.edu>
2025-11-21 21:30:13 -0500

    better clickables

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,36 @@ function toggleMenu() {
 	document.body.classList.toggle('open');
 	header.classList.toggle('open')
 }
+
+// Desktop dropdown functionality
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+	const dropdownLink = dropdown.querySelector('a');
+	
+	if (dropdownLink) {
+		dropdownLink.addEventListener('click', (evt) => {
+			evt.preventDefault();
+			evt.stopPropagation();
+			
+			// Close all other dropdowns
+			dropdowns.forEach(otherDropdown => {
+				if (otherDropdown !== dropdown) {
+					otherDropdown.classList.remove('open');
+				}
+			});
+			
+			// Toggle current dropdown
+			dropdown.classList.toggle('open');
+		});
+	}
+});
+
+// Close dropdowns when clicking outside
+document.addEventListener('click', (evt) => {
+	if (!evt.target.closest('.dropdown')) {
+		dropdowns.forEach(dropdown => {
+			dropdown.classList.remove('open');
+		});
+	}
+});
